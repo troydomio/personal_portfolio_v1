620b3aae523f01ac1a2a15a4b1fc50d71b5acd26
Troy <troyakins99@yahoo.com>
2021-08-15 20:34:51 -0500

    added youtube icon link

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,5 +1,7 @@
 import github from '../components/git.png'
 import linkedin from '../components/lin.png'
+import youtube from '../components/youtube.png'
+
 
 
 const Intro = () => {
@@ -11,7 +13,9 @@ const Intro = () => {
                 <p>Howdy,<br/><br/>I'm <span className="name"> Troy Domio</span>, a full-stack web developer based out of Dallas, Texas. 
                 <br/>
                 <a href="https://github.com/troydomio" target="_blank"><img className="git" src={github} ></img></a>
-          <a href="https://www.linkedin.com/in/troydomio/" target="_blank"><img className="linkedin" src={linkedin} ></img></a></p>  
+          <a href="https://www.linkedin.com/in/troydomio/" target="_blank"><img className="linkedin" src={linkedin} ></img></a>
+          <a href="https://www.youtube.com/channel/UC6IohvICPLUWWUmaDadtF_w" target="_blank"><img className="youtube" src={youtube} ></img></a>
+          </p>  
             </div>
             </div>
         </section>
