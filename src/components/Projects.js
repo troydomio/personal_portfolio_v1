e0922da442109d937273b690cc648078d7419a08
Troy <troyakins99@yahoo.com>
2021-10-24 00:05:18 -0500

    updated projects list

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Projects = () => {
     return (
         
-        <div className="projectportfolio">
+       
        
             
              <div className="project-container">
@@ -15,6 +15,15 @@ const Projects = () => {
             <p>This project is a CRUD (create, read, update, delete) full-stack application where users can perform all of the basic operations of persistent storage. </p>
             <a href="https://murmuring-ridge-09826.herokuapp.com/"  target="_blank" className="click">DEMO</a>
             <a href="https://github.com/troydomio/potato-games" className="click" target="_blank" >CODE</a>
+            </div>
+
+            <div className="project-cards">
+            <p>Page Visitor Counter</p>
+            <p>React + Rails</p>
+            <a href="https://glacial-plains-09503.herokuapp.com/" target="_blank" ><img className="project-img" src="https://live.staticflickr.com/65535/51624165676_5549ea7eab_b.jpg" ></img></a>
+            <p>This project counts the views of visitors and updates the view count in the rails api backend. </p>
+            <a href="https://glacial-plains-09503.herokuapp.com/"  target="_blank" className="click">DEMO</a>
+            <a href="https://github.com/troydomio/viewcounter_v3" className="click" target="_blank" >CODE</a>
             </div>
             
             <div className="project-cards">
@@ -31,14 +40,13 @@ const Projects = () => {
             <p>New Years Countdown Timer</p>
             <p>JavaScript + CSS + HTML</p>
             <a href="https://troydomio.github.io/newyearscountdown/" target="_blank"><img className="project-img" src="https://live.staticflickr.com/65535/51598123108_7e33bc0ba9_b.jpg"></img></a>
-
             <p>This project is a simple timer counting down the days, hours, minutes and seconds until January 1, 2022!</p>
             <a href="https://troydomio.github.io/newyearscountdown/" target="_blank" className="click">DEMO</a>
             <a href="https://github.com/troydomio/newyearscountdown" target="_blank"className="click">CODE</a>
             </div>
 
             </div>
-        </div>
+        
     )
 }
 
